38d6f7ccbbffc277b9e86c0501fa9adf0a5948e8
burak emre dost <burakemredost@gmail.com>
2012-11-04 19:55:52 +0200

    yazdırma düzenlendi
    yazdırma düzenlendi

--- src/balon.java
+++ src/balon.java
@@ -6,43 +6,64 @@ public class balon {
 
 	public static void main(String[] args) {
 		
-		List linked =  new List();
+		List kat3 =  new List();
+		List kat6 =  new List();
+		List kat9 =  new List();
 		
 		String[] renk = {"Kirmizi","Yesil","Mavi","Sari","Mor","Turuncu","Pembe","Gri","Siyah","Beyaz"};
 		
-		linked.ekle(renk);
-		linked.display();
-		System.out.println();
-		
+		kat3.ekle(renk);
+		kat6.ekle(renk);
+		kat9.ekle(renk);
 		
 		Stack<araba> kat2=new Stack<araba>();
 		Stack<araba> kat5=new Stack<araba>();
 		Stack<araba> kat8=new Stack<araba>();
+		
 		add_stack(renk,kat2);
-		stack_yazdir(kat2);
-		System.out.println();
 		add_stack(renk,kat5);
-		stack_yazdir(kat2);
-		System.out.println();
-		add_stack(renk,kat8);
-		stack_yazdir(kat2);
-		System.out.println();
+        add_stack(renk,kat8);
+		
+	
 		
 		Queue<araba> kat1=new LinkedList<araba>();
 		Queue<araba> kat4=new LinkedList<araba>();
 		Queue<araba> kat7=new LinkedList<araba>();
+		
 		add_queue(renk,kat1);
-		queue_yazdir(kat1);
-		System.out.println();
+		add_queue(renk,kat4);
 		add_queue(renk,kat7);
-		queue_yazdir(kat1);
+		
+		System.out.println("KAT 1");
+        queue_yazdir(kat1);
 		System.out.println();
+		System.out.println("KAT 2");
+		stack_yazdir(kat2);
+		System.out.println();
+		System.out.println("KAT 3");
+		kat3.display();
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("KAT 4");
 		queue_yazdir(kat4);
 		System.out.println();
+		System.out.println("KAT 5");
+		stack_yazdir(kat5);
+		System.out.println();
+		System.out.println("KAT 6");
+		kat6.display();
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("KAT 7");
 		queue_yazdir(kat7);
-		
-		
-		
+		System.out.println();
+		System.out.println("KAT 8");
+		stack_yazdir(kat8);
+		System.out.println();
+		System.out.println("KAT 9");
+		kat9.display();
 	}
 	
 	public static void add_queue(String[] r,Queue<araba> kat){
@@ -57,7 +78,7 @@ public static void add_stack(String[] r,Stack<araba> eklenen){
 		
 		for(int i=0;i<10;i++){
 			araba a1=new araba(r[i]);
-			eklenen.add(a1);
+			eklenen.push(a1);
 			
 		}
 	}
